71acbb7e1f03e810d02d5e2955125d5ed2629db8
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-16 02:54:25 +0300

    style: fix nav link styles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
-      log_in user
+      log_in_user user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,5 +15,10 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def log_in_user(user)
+    session[:user_id] = user.id
   end
 end
